5c42ed5267d58f78b0c6ce2e88eb9fd5fe9b4b6e
Mohammed Yaba <91971316+Gardi95@users.noreply.github.com>
2022-06-29 23:11:56 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci_numbers_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, n;
+            int a = 0, b = 1, c = 0;
+
+            Console.WriteLine("Enter the Numer: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+          
+
+            while (i <= n)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                i = i + 1;
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
